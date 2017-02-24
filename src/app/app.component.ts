534348a62d8ff041c2cf1413ed2f1e312f625cff
Pubudu Ranasinghe <pubudutr@gmail.com>
2017-02-24 13:23:36 +0530

    article list

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Article } from './article/article.model'
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,22 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app works!';
+  title = 'ng2News';
+  articles: Article[];
+
+  constructor() {
+    this.articles = [
+      new Article('Angular 2', 'http://angular.io', 3),
+      new Article('Angular 1', 'http://angularjs.org', 2),
+      new Article('Google', 'http://www.google.com', 1)
+    ];
+  }
+
+  addArticle(title: HTMLInputElement, link: HTMLInputElement): boolean {
+      console.log(`Adding article: //${title.value} //${link.value}`);
+      this.articles.push(new Article(title.value, link.value, 0));
+      title.value = '';
+      link.value = '';
+      return false;
+  } 
 }
